38b601590dc40f15df2a2fb72202387a470ab6aa
MakarovOleg1988 <makarov.oleg1988@yandex.ru>
2022-09-25 11:57:14 +0300

    Урок Массивы

---
+++ Lesson11/Lesson11.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace Lesson11
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            _arrayMethod();
+        }
+
+        public static void _arrayMethod()
+        {
+            int[] _array = new int[5]; //массив   
+
+            var _size_array = _array.Length; //длина массива
+
+            _array[1] = 5;
+
+            var _oldArray = _array;
+            _array = new int[20]; //обновление ссылки на массив
+
+            //_array = null; обнуление массива
+            //_array = default; обнуление массива через default
+
+            long[] _checkArray = null;
+
+            Array _t = _oldArray;
+
+            foreach (var r in _array) Console.WriteLine(r);
+            //[0][1][2][3][4] индексация ячеек массива
+        }
+    }
+}
